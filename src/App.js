764d7b9d36805a2df1171053124b9c3d6616b1c0
kototo522 <huyejiben@gmail.com>
2021-01-23 13:01:35 +0900

    送信ボタンの機能追加

--- src/App.js
+++ src/App.js
@@ -1,70 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
 
 
+const Appbackground = styled.div`
+  background-color: #e7b09f;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`;
 
 const Coffee = styled.img`
-  margin-bottom: 75px;
-  display: center;
+  display: flex;
+  position: center;
   top: 30px;
-  left: 0;
   height: 650px;
-  width: 875px;
+  width: 100vw;
+  object-fit: contain;
 `;
-
-const Pentab = styled.img`
-  margin-left: 1150px;
-  margin-top: 45px;
-  height: 30px;
-  cursor: pointer;
-`;
-
-const Clear = styled.img`
-  position: fixed;
-  margin-top: 60px;
-  margin-left: 300px;
-  cursor: pointer;
-  z-index: 5;
-  height: 50px;
-  width: 50px;
-`
-
 
 const Appheader = styled.div`
+  position: relative;
   background-color: #c39c89;
+  margin-top: 0px;
   height: 80px;
   width: 100%;
-  position: relative;
 `;
 
 const HeaderText = styled.text`
   position: fixed;
   height: 30px;
-  top: 20px;
+  top: 10px;
   margin-left: 70px;
   font-size: 37px;
   color: white;
+  z-index: 1;
 `;
 
-const Appbackground = styled.div`
-  background-color: #e7b09f;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
-
-const Biglogo = styled.text`
-  position: relative;
-  top: 230px;
-  left: 33px;
-  vertical-align: middle;
-  padding: 500px;
-  font-size: 250px;
-  color: #c39c89;
+const Pentab = styled.img`
+  position: absolute;
+  top: 50%;
+  right: 10%;
+  height: 30px;
+  cursor: pointer;
+  transform: translateY(-50%);
 `;
 
 const Main = styled.div`
@@ -75,16 +56,26 @@ const Main = styled.div`
   height: calc(100vh - 80px);
   width: 100vw;
   overflow-y: scroll;
+  overflow-x: hidden;
+`;
+
+const Biglogo = styled.div`
+  position:absolute;
+  top: 230px;
+  left: 50%;
+  font-size: 250px;
+  color: #c39c89;
+  transform: translateX(-50%);
 `;
 
 const SampleBox = styled.div`
   position: relative;
   top: 450px;
-  margin: 80px auto 0;
   height: 200px;
   width: 700px;
   border-radius: 10px;
   background-color: rgba(238, 226, 211, 0.8);
+  transform: translateX(50%);
   &::after {
     position: relative;
     top: 200px;
@@ -94,29 +85,39 @@ const SampleBox = styled.div`
   }
 `;
 
-const Frombackground=styled.div`
-position: fixed;
-background-color: rgba(149, 126, 128, 0.75);
-z-index: 3;
-min-height: 100vh;
-width: 100%;
-`
-
+const Frombackground = styled.div`
+  position: fixed;
+  background-color: rgba(149, 126, 128, 0.75);
+  z-index: 3;
+  min-height: 100vh;
+  width: 100%;
+`;
 
 const Textarea = styled.textarea`
-    position: fixed;
-    vertical-align: middle;
-    margin-left: 400px;
-    margin-top: 90px;
-    border-radius: 10px;
-    border: 1px solid black;
-    height: 400px;
-    width: 600px;
-    background-color: #c39c89;
-    border: white;
-    font-size: 35px;
-    color: #eee2d3;
-`
+  position: fixed;
+  vertical-align: middle;
+  margin-left: 400px;
+  margin-top: 90px;
+  border-radius: 10px;
+  border: 1px solid black;
+  height: 400px;
+  width: 600px;
+  background-color: #c39c89;
+  border: white;
+  font-size: 35px;
+  color: #eee2d3;
+  box-shadow: white;
+`;
+
+const Clear = styled.img`
+  position: fixed;
+  margin-top: 60px;
+  margin-left: 300px;
+  cursor: pointer;
+  z-index: 4;
+  height: 50px;
+  width: 50px;
+`;
 
 const Inputbutton = styled.input`
   position: fixed;
@@ -128,83 +129,52 @@ const Inputbutton = styled.input`
   color: #c39c89;
   background-color: #eee2d3;
   border: white;
-  z-index: 100;
-`
+  z-index: 5;
+`;
 
 function App() {
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [info, setInfo] = useState("");
+  const [infoList, setinfoList] = useState([]);
 
-  const [Page, setPage] = useState(1);
-  const page = () => {
-    setPage(2);
-  }
-
-
-  const depage = () => {
-    setPage(1);
-  }
-
-
-  const [Delete, setDelete] = useState(3);
-  const clear = () => {
-    setDelete(4);
-}
-
-
-
-  const [showModal, setShowModal] = useState(false);
-
-  const SubmitForm = () => {
-      setShowModal(true);
-  }
-
-
-  const [textarea, setText] = useState('');
-
-
+  const addInfoList = (info) => {
+    setinfoList([...infoList, info]);
+    setIsOpenModal(false);
+  };
 
   return (
     <div>
+      <Appbackground>
       <Appheader>
         <HeaderText>blog</HeaderText>
-        <Pentab 
-          src={"./images/pen.svg"} onClick = {() => setPage('page')}></Pentab>
-          {Page === 1
-          ? <div/>
-          : <Frombackground>
+      <Appheader/>
+        <Pentab src={"./images/pen.svg"} onClick={() => setIsOpenModal(true)}/>
+        {isOpenModal && (
+          <Frombackground>
+            <Inputbutton value="shere" onClick={() => addInfoList(info)} />
 
-         <Inputbutton type = "submit" value="shere"/>
+            <Clear
+              src={"./images/clear.svg"}
+              onClick={() => setIsOpenModal(false)}
+            />
 
-        
-        <Clear
-          src={"./images/clear.svg"} onClick = {() => depage()}></Clear>
-           {Delete === 3
-           ? <div/>
-           : <SubmitForm cancel = {() => setShowModal(false)}/>
-          }   
-
-              <Textarea row="10" cols="20">おすすめのスイーツやカフェの情報を共有しよう！
-              
-              </Textarea>
-
+            <Textarea
+              row="10"
+              cols="20"
+              placeholder={"おすすめのスイーツやカフェの情報を共有しよう！"}
+              value={info}
+              onChange={(e) => setInfo(e.target.value)}
+            />
           </Frombackground>
-          }
-
+        )}
       </Appheader>
-      <Appbackground>
         <Coffee src={"./images/coffee.jpg"} />
       </Appbackground>
       <Main>
         <Biglogo>text</Biglogo>
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
-        <SampleBox />
+        {infoList.map((info) => (
+          <SampleBox>{info}</SampleBox>
+        ))}
       </Main>
     </div>
   );
